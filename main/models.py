e2e53558a2978111d96def3c67fb9f155f34f82e
Carlo Arellano <carlo.arellano@g.msuiit.edu.ph>
2023-07-11 15:17:48 +0800

    commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ToDoList(models.Model):
+    name = models.charFeild(max_Length=200)
+    
+    def __str__ (self):
+        return self.name
+    
+class Item(models.Model):
+    ToDoList = models.models.ForeignKey(ToDoList, on_delete = models.CASCADE)
+    text = models.CharFeild(max_Length=300)
+    complete = models.BooleanField()
+    
+    def __str__(self):
+        return self.text 
